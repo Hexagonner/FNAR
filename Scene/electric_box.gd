6ef0fc7adb8e8f0f4a22e0a1ed288eff33303a15
hexagonner <snipewo@gmail.com>
2026-07-01 16:53:28 +0900

    fusebox_v11

--- Scene/electric_box.gd
+++ Scene/electric_box.gd
@@ -1,5 +1,17 @@
 extends CSGBox3D
 
+var index_arr:Array
+var pin_arr:Array
+
 func _ready() -> void:
-	pass
+	for pin in get_tree().get_nodes_in_group("pins"):
+		if pin.get("pin_type") == "OUT":
+			index_arr.append(pin.global_position)
+			pin_arr.append(pin)
+		#is_in_group("pins")
+	print(index_arr)
+	index_arr.shuffle()
+	print(index_arr)
+	for i in range(pin_arr.size()):
+		pin_arr[i].global_position = index_arr[i]
 	
--- Scene/wirepin.gd
+++ Scene/wirepin.gd
@@ -4,6 +4,15 @@ class_name WirePin
 
 @export_enum("IN", "OUT") var pin_type: String = "IN"
 @export_enum("a", "b", "c", "d") var pin_num: int = 0
-
 # [추가] 이 핀에 연결된 와이어 객체를 저장 (null이면 빈 핀)
 var connected_wire: Node3D = null
+#static var index_arr:Array
+#func _ready() -> void:
+	#
+	#if pin_type == "OUT":
+		#self_pos = self.global_position
+		#index_arr.append(self_pos)
+		#index_arr.shuffle()
+		#print(index_arr)
+		#is_in_group("pins")
+		#
